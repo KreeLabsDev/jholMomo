8c993cb655cc4ae6c709288f7a513a15eed238b3
nxhettry <nishan.gtsn@gmail.com>
2025-04-30 16:45:24 +0545

    created first route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center text-black">
-      <h1 className="text-3xl font-sans">Welcome to चाराली झोल म:म:</h1>
-      <Image
-        src="/banner.png"
-        alt="Khata Logo"
-        height={300}
-        width={300}
-        className="rounded-full shadow-lg"
-      />
-      <Link href="/dashboard">
-        <Button>Get Started</Button>
+    <div className="w-full h-full flex flex-col justify-around items-center text-black">
+      <div className="flex flex-col justify-center items-center gap-8">
+        <Image
+          src="/banner.png"
+          alt="Khata Logo"
+          height={300}
+          width={300}
+          className="rounded-full shadow-lg"
+        />
+        <h1 className="text-3xl font-sans font-semibold">Welcome to चाराली झोल म:म:</h1>
+
+      </div>
+      <Link className="w-xs" href="/dashboard">
+        <Button className="w-full h-16 text-lg">Get Started</Button>
       </Link>
     </div>
   );
