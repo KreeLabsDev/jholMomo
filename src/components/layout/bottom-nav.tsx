eeaee5c7e335ef96aad0458278b3e8a497a81d2f
nxhettry <nishan.gtsn@gmail.com>
2025-04-30 21:59:48 +0545

    finished the bottom nav

--- src/components/layout/bottom-nav.tsx
+++ src/components/layout/bottom-nav.tsx
@@ -1,11 +1,15 @@
 "use client";
 import React from "react";
 import { MdHome } from "react-icons/md";
-import { FaHistory } from "react-icons/fa";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaHistory, FaShoppingCart, FaPlus } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
-import { FaPlus } from "react-icons/fa";
-import { usePathname } from "next/navigation";
+import Link from "next/link";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
 
 const NAV_ITEMS = [
   {
@@ -32,27 +36,57 @@ const NAV_ITEMS = [
 ];
 
 const BottomNav = () => {
-  const pathname = usePathname();
 
   return (
-    <div className="w-full h-16 bg-white shadow-md border flex items-center justify-between px-4 py-2">
-      {NAV_ITEMS.map((item, index) => {
-        return item.name !== "Add" ? (
+    <div className="w-full max-h-20 relative bg-white shadow-md border flex justify-around items-center px-4 py-2">
+      {NAV_ITEMS.map((item) => {
+        return item.name === "Add" ? (
+          <Link href="/order" key={item.name} className="flex flex-col justify-center items-center h-16 w-16 p-3 bg-black rounded-full hover:bg-gray-700">
+            <FaPlus className="text-2xl text-gray-200" />
+          </Link>
+        ) : (
+          <Link
+            href={item.path as string}
+            key={item.name}
+            className={`w-10 flex flex-col justify-center items-center p-1 rounded-full hover:bg-gray-200`}
+          >
+            {item.icon}
+            <p className="text-xs mt-1">{item.name}</p>
+          </Link>
+        );
+      })}
+
+      {/* {NAV_ITEMS.map((item) => {
+        return item.name === "Add" ? (
+          <div key=""></div>
+        ) : (
           <div
-            key={index}
-            className={`h-full p-1 rounded-full flex flex-col justify-between items-center hover:bg-gray-200 ${
+            key={item.name}
+            className={`h-12 flex flex-col justify-center items-center p-1 rounded-full hover:bg-gray-200 ${
               pathname === item.path ? "bg-gray-200" : ""
             }`}
           >
             {item.icon}
-            <p className="text-xs">{item.name}</p>
-          </div>
-        ) : (
-          <div key={item.name} className="mb-10 h-16 w-16 p-1 bg-black rounded-full flex flex-col justify-around items-center">
-            <FaPlus className="text-2xl text-gray-200" />
+            <p className="text-xs mt-1">{item.name}</p>
           </div>
         );
-      })}
+      })} */}
+
+      {/* <div className="absolute left-1/2 bottom-5 transform -translate-x-1/2">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <div className="flex flex-col justify-center items-center h-16 w-16 p-3 bg-black rounded-full hover:bg-gray-700">
+              <FaPlus className="text-2xl text-gray-200" />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center">
+            <DropdownMenuItem>Sales</DropdownMenuItem>
+            <DropdownMenuItem>Party</DropdownMenuItem>
+            <DropdownMenuItem>Stock</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div> */}
     </div>
   );
 };
