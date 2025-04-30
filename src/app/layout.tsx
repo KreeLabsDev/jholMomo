660e5ee00193133b3427df9c7657bf7f0a53f528
nxhettry <nishan.gtsn@gmail.com>
2025-04-30 16:40:37 +0545

    creating landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
+
+const ubuntu = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-ubuntu",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Khata",
@@ -13,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen w-screen">{children}</body>
+      <head></head>
+      <body className={`h-screen w-screen ${ubuntu.className}`}>
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className="w-full h-full flex flex-col justify-center items-center text-black">
+      <h1 className="text-3xl font-sans">Welcome to चाराली झोल म:म:</h1>
+      <Image
+        src="/banner.png"
+        alt="Khata Logo"
+        height={300}
+        width={300}
+        className="rounded-full shadow-lg"
+      />
+      <Link href="/dashboard">
+        <Button>Get Started</Button>
+      </Link>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
