582d4c0e6a2c5cf6392470c6e94cdd3d71a3a0be
nxhettry <nishan.gtsn@gmail.com>
2025-05-01 11:22:28 +0545

    created an avatar

---
+++ src/app/(bottomnav)/AvatarSection.tsx
@@ -0,0 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const AvatarSection = () => {
+  return (
+    <div className="absolute top-4 right-4 z-10">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src="/avatar.svg" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default AvatarSection;
--- src/app/(bottomnav)/layout.tsx
+++ src/app/(bottomnav)/layout.tsx
@@ -1,4 +1,5 @@
 import BottomNav from "@/components/layout/bottom-nav";
+import AvatarSection from "./AvatarSection";
 
 export default function RootLayout({
   children,
@@ -6,7 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-gray-50 w-full h-full flex flex-col  justify-center items-center">
+    <div className="bg-gray-50 w-full h-full flex flex-col  justify-center items-center relative">
+      <div className="absolute top-4 right-4 z-10">
+        <AvatarSection />
+      </div>
+
       <div className="flex-1 p-3">{children}</div>
       <BottomNav />
     </div>
